53b684b73d470ef796f6361f05c19d60696c38dd
Joseph <tjaduwari@hotmail.com>
2020-11-11 02:02:38 +1030

    final before conversion to ejs and node

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/main.css">
+    <title></title>
+  </head>
+  <body>
+    <nav>
+      <h1 id="site-header">Demo Site</h1>
+      <ul class="nav-list">
+        <li class="nav-item"><a>Home</a></li>
+        <li class="nav-item"><a>About</a></li>
+        <li class="nav-item"><a>Contacts</a></li>
+      </ul>
+    </nav>
+    <main>
+      <section class="content">
+        <h2 class="page-heading">Section heading</h2>
+      </section>
+    </main>
+    <footer>
+      <p>&copy; 2020 </p>
+    </footer>
+  </body>
+</html>
