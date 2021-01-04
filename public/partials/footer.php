433f8722aa82fa0889b5411a4af62d68c353b967
Joseph <tjaduwari@hotmail.com>
2021-01-05 06:46:13 +1030

    ready

---
+++ public/partials/footer.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="footer-flex">
+    <section class="footer-links">
+      <ul>
+        <li>Site map</li>
+      </ul>
+    </section>
+    <section class="contact-details" id="contacts">
+      <h4>Contact Us</h4>
+      <ul>
+        <li>Phone: </li>
+        <li>E-mail: </li>
+      </ul>
+    </section>
+  </div>
+  <p>&copy; 2020 PHANEROS GROUP</p>
+</footer>
+</body>
+</html>
---
+++ public/partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/main.css">
+    <title><?php echo TITLE; ?></title>
+  </head>
+  <body>
+    <nav>
+      <h1 id="site-header"><a href="/"><img src="public/icons/logo.svg" alt="phaneros logo"></a></h1>
+      <ul class="nav-list">
+        <li class="nav-item"><a href="/">Home</a></li>
+        <li class="nav-item"><a href="/products.php">Products</a></li>
+        <li class="nav-item"><a href="/about.php">About</a></li>
+        <li class="nav-item"><a href="#contacts">Contacts</a></li>
+      </ul>
+    </nav>
